1f7e7b036b49fa54b7aa578512449d79bf5b599f
Sonali Patil <sonali.patil@communitybrands.com>
2022-03-04 19:28:30 +0530

    first commit

--- src/main/java/com/Bookzy/Bookzy/BookzyApplication.java
+++ src/main/java/com/Bookzy/Bookzy/BookzyApplication.java
@@ -1,4 +1,4 @@
-package com.Bookzy.Bookzy;
+package com.bookzy.Bookzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/Bookzy/Bookzy/BookzyApplicationTests.java
+++ src/test/java/com/Bookzy/Bookzy/BookzyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Bookzy.Bookzy;
+package com.bookzy.Bookzy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
